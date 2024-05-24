5e127e227de854d3e6cbc09574e5c87f97158eb9
Rohid <rsewpersd@syntess.nl>
2024-05-24 16:11:56 +0200

    Changed some minor things

--- tsom-bot/FetchGuildData.ts
+++ tsom-bot/FetchGuildData.ts
@@ -4,29 +4,33 @@ export class FetchGuildData {
         const url = "http://localhost:3000/guild";
 
         const body = {
-            payload: {
-                guildId: "l943tTO8QQ-_IwWHfwyJuQ",
-                includeRecentGuildActivityInfo: true
+            "payload": {
+                "guildId": "l943tTO8QQ-_IwWHfwyJuQ",
+                "includeRecentGuildActivityInfo": true
             },
             enums: false
         }
         console.log(body);
 
-        const requestHeaders: HeadersInit = new Headers();
-        requestHeaders.set('content-type', 'application/json; charset=utf-8');
-        requestHeaders.set('Origin', 'http://localhost:5500');
-
         let result;
+
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
         try {
             result = await fetch(url, {
                 method: 'POST',
-                headers: requestHeaders,
+                // headers: {
+                // "content-type": "application/json; charset=utf-8",
+                // "Origin": "http://localhost:5000"
+                // },
+                headers: headers,
                 body: JSON.stringify(body),
             })
         } catch (error) {
             console.log(error);
         }
+        console.log(result);
         return await result.json();
     }
     constructor() {

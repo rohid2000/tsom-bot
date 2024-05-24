dc7b0ce2598f9105a0e4e1a19f0f2f898f1d726d
Rohid <rsewpersd@syntess.nl>
2024-05-24 15:23:32 +0200

    file location refactor and added eventListener

---
+++ tsom-bot/FetchGuildData.ts
@@ -0,0 +1,31 @@
+export class FetchGuildData {
+
+    public async fetchGuildData(): Promise<Response> {
+        const url = "http://localhost:3000/guild";
+
+        const body = {
+            payload: {
+                guildId: "l943tTO8QQ-_IwWHfwyJuQ",
+                includeRecentGuildActivityInfo: true
+            },
+            enums: false
+        }
+        console.log(body);
+
+        const requestHeaders: HeadersInit = new Headers();
+        requestHeaders.set('content-type', 'application/json; charset=utf-8');
+        requestHeaders.set('Origin', 'http://localhost:5500');
+
+
+        const result = await fetch(url, {
+            method: 'POST',
+            headers: requestHeaders,
+            body: JSON.stringify(body),
+        })
+        return await result.json();
+    }
+
+    constructor() {
+        console.log();
+    }
+}
--- tsom-bot/app.ts
+++ tsom-bot/app.ts
@@ -1,5 +1,11 @@
-import { FetchGuildData } from "./ticket-tracker/FetchGuildData.js";
+import { FetchGuildData } from "./FetchGuildData.js";
 
 const guildData = new FetchGuildData();
 
 console.log(guildData.fetchGuildData());
+
+const fetchData = document.getElementById("fetchButton");
+
+fetchData.addEventListener("click", async () => {
+    console.log(await guildData.fetchGuildData());
+});

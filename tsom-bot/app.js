6a824a9b4a5ebf044608282f7d5107d02d7dfed8
Yannick de Zwart <yannickdezwart@gmail.com>
2024-06-13 16:45:28 +0200

    Replace commit

--- tsom-bot/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const FetchGuildData_1 = require("./ticket-tracker/FetchGuildData");
-const guildData = new FetchGuildData_1.GuildData();
-console.log(guildData.fetchGuildData());
-//# sourceMappingURL=app.js.map

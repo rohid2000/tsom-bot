163cdb8d7d15e83507beb3c817e7a54a74804b22
010Jonathan <j.desnoo1000@hotmail.nl>
2024-06-15 20:48:54 +0200

    Initialized a discord bot and intergrated it with the API

--- Program.cs
+++ Program.cs
@@ -1,24 +1,61 @@
 using System.Net.Http.Json;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using tsombot.config;
 
-await ProcessRepositoriesAsync();
-
-static async Task ProcessRepositoriesAsync()
+internal class Program
 {
-    HttpClient client = new HttpClient();
+    private static DiscordClient client { get; set; }
+    private static CommandsNextExtension commands { get; set; }
 
-    var jsonContent = JsonContent.Create(new
+    private static async Task Main(string[] args)
     {
-        payload = new
+        //Initialize Discord Bot
+        var configReader = new ConfigReader();
+        await configReader.readConfig();
+
+        var discordConfig = new DiscordConfiguration()
         {
-            guildId = "l943tTO8QQ-_IwWHfwyJuQ",
-            includeRecentGuildActivityInfo = true
-        },
-        enums = false
-    });
+            Intents = DiscordIntents.All,
+            Token = configReader.token,
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+        };
+
+        client = new DiscordClient(discordConfig);
+
+        client.Ready += Client_Ready;
+
+        await client.ConnectAsync();
+
+        Task apiTask = Task.Run(ProcessRepositoriesAsync);
+
+        await Task.Delay(-1);
+    }
+
+    static async Task ProcessRepositoriesAsync()
+    {   
+        HttpClient client = new HttpClient();
+
+        var jsonContent = JsonContent.Create(new
+        {
+            payload = new
+            {
+                guildId = "l943tTO8QQ-_IwWHfwyJuQ",
+                includeRecentGuildActivityInfo = true
+            },
+            enums = false
+        });
 
 
-    var response = await client.PostAsync("https://swgoh-comlink-latest-nfw1.onrender.com/guild", jsonContent);
-    var responseString = await response.Content.ReadAsStringAsync();
+        var response = await client.PostAsync("https://swgoh-comlink-latest-nfw1.onrender.com/guild", jsonContent);
+        var responseString = await response.Content.ReadAsStringAsync();
 
-    Console.WriteLine(responseString);
+        Console.WriteLine(responseString);
+    }
+
+    private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
 }
---
+++ config/ConfigReader.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace tsombot.config
+{
+    public class ConfigReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task readConfig()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                ConfigStructure data = JsonConvert.DeserializeObject<ConfigStructure>(json);
+
+                if (data != null)
+                {
+                    this.token = data.token;
+                    this.prefix = data.prefix;
+                }
+            }
+        }
+    }
+
+    internal sealed class ConfigStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}

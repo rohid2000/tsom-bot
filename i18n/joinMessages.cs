923f155c2e4e3203a324af25e3e8d3d3a632a81c
Rohid <rohid10@live.be>
2024-12-20 17:40:36 +0100

    Added joinMessages.cs

---
+++ i18n/joinMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsom_bot.i18n
+{
+    public class joinMessages
+    {
+
+    }
+}
